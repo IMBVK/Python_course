0cfb566f6e8a7b0f5013e7293bdf98a2d025af2d
Varun Bhandekar <95991988+IMBVK@users.noreply.github.com>
2022-04-27 18:06:54 +0530

    Add files via upload

---
+++ sep_end_method.py
@@ -0,0 +1,12 @@
+# print('h',end='-')
+# print('i')
+
+# print('hell',end='')
+# print('o')
+
+# print(1,2,3,4,5,sep='\n')
+# print(1,2,3,4,5,sep='-',end='')
+
+# print('h','e','l','l','o',sep='%')
+
+print('varun','bhandekar',sep='-',end='-kumar')
