289659dd228abcdec44bea8dbecfa8e61397278f
Varun Bhandekar <95991988+IMBVK@users.noreply.github.com>
2022-05-05 18:38:07 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,8 @@
+name=input('Enter your Name: ')
+scname=input('Enter your college Name: ')
+rno=input('Enter your Roll no.: ')
+p= input('Enter your Perentage: ')
+h=float(input('Enter your height: '))
+m=input('Enter your favourite Movie: ')
+
+print('My name is', name, 'college name is', scname, 'Roll no.:', rno,  '\nPercentage is', p+'%', "Height is", h, 'My favourite movie is', m)
