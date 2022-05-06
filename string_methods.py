f772c5fd784ac29da128fc95564f25d0a8f8424a
Varun Bhandekar <95991988+IMBVK@users.noreply.github.com>
2022-05-06 20:16:23 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,22 @@
+"""Count method"""
+
+# a="I love to code in python, so im learning python"
+# b=a.count('python')
+# print(b)
+
+'''find method'''
+
+# a = "Hello, welcome to python "
+# b = a.find("python")
+# print(b)
+
+'''Index method'''  #it is same as find method
+# a = "Hello, welcome to python "
+# b = a.index("python")
+# print(b)
+
+'''Casefold'''
+
+# print('Programming'.casefold())
+# print('PrOGramming'.casefold())
+print('PROGramming'.casefold())
